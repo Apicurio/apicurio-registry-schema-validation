cee50c2f8d203808b685adfbcc3714eeb943be1c
Carles Arnal <carlesarnal92@gmail.com>
2024-04-17 10:15:01 +0200

    Use network nt for schema validation

--- jsonschema/src/main/java/io/apicurio/schema/validation/json/JsonValidator.java
+++ jsonschema/src/main/java/io/apicurio/schema/validation/json/JsonValidator.java
@@ -16,20 +16,12 @@
 
 package io.apicurio.schema.validation.json;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
 import io.apicurio.registry.resolver.DefaultSchemaResolver;
 import io.apicurio.registry.resolver.ParsedSchema;
 import io.apicurio.registry.resolver.SchemaLookupResult;
@@ -39,6 +31,17 @@ import io.apicurio.registry.resolver.SchemaResolverConfig;
 import io.apicurio.registry.resolver.data.Record;
 import io.apicurio.registry.resolver.strategy.ArtifactReference;
 import io.apicurio.registry.types.ArtifactType;
+import io.apicurio.registry.utils.IoUtil;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Provides validation APIs for JSON objects (Java objects, byte[], StringJSONObject,...) against a JSON Schema.
@@ -48,13 +51,14 @@ import io.apicurio.registry.types.ArtifactType;
  */
 public class JsonValidator {
 
-    private SchemaResolver<Schema, Object> schemaResolver;
+    private SchemaResolver<JsonSchema, Object> schemaResolver;
     private ArtifactReference artifactReference;
 
     /**
      * Creates the JSON validator.
      * If artifactReference is provided it must exist in Apicurio Registry.
-     * @param configuration , configuration properties for {@link DefaultSchemaResolver} for config properties see {@link SchemaResolverConfig}
+     *
+     * @param configuration     , configuration properties for {@link DefaultSchemaResolver} for config properties see {@link SchemaResolverConfig}
      * @param artifactReference , optional {@link ArtifactReference} used as a static configuration to always use the same schema for validation when invoking {@link JsonValidator#validateByArtifactReference(Object)}
      */
     public JsonValidator(Map<String, Object> configuration, Optional<ArtifactReference> artifactReference) {
@@ -70,13 +74,14 @@ public class JsonValidator {
     /**
      * Validates the provided object against a JSON Schema.
      * The JSON Schema will be fetched from Apicurio Registry using the {@link ArtifactReference} provided in the constructor, this artifact must exist in the registry.
+     *
      * @param bean, the object that will be validate against the JSON Schema, can be a custom Java bean, String, byte[], InputStream, {@link JSONObject} or Map.
      * @return JsonValidationResult
      */
     public JsonValidationResult validateByArtifactReference(Object bean) {
         Objects.requireNonNull(this.artifactReference, "ArtifactReference must be provided when creating JsonValidator in order to use this feature");
-        SchemaLookupResult<Schema> schema = this.schemaResolver.resolveSchemaByArtifactReference(this.artifactReference);
-        JSONObject jsonPayload = createJSONObject(bean);
+        SchemaLookupResult<JsonSchema> schema = this.schemaResolver.resolveSchemaByArtifactReference(this.artifactReference);
+        JsonNode jsonPayload = createJSONObject(bean);
         return validate(schema.getParsedSchema().getParsedSchema(), jsonPayload);
     }
 
@@ -85,76 +90,81 @@ public class JsonValidator {
      * This method will resolve the schema based on the configuration provided in the constructor. See {@link SchemaResolverConfig} for configuration options and features of {@link SchemaResolver}.
      * You can use {@link JsonRecord} as the implementation for the provided record or you can use an implementation of your own.
      * Opposite to {@link JsonValidator#validateByArtifactReference(Object)} this method allow to dynamically use a different schema for validating each record.
+     *
      * @param record , the record used to resolve the schema used for validation and to provide the payload to validate.
      * @return JsonValidationResult
      */
     public JsonValidationResult validate(Record<Object> record) {
-        SchemaLookupResult<Schema> schema = this.schemaResolver.resolveSchema(record);
-        JSONObject jsonPayload = createJSONObject(record.payload());
+        SchemaLookupResult<JsonSchema> schema = this.schemaResolver.resolveSchema(record);
+        JsonNode jsonPayload = createJSONObject(record.payload());
         return validate(schema.getParsedSchema().getParsedSchema(), jsonPayload);
     }
 
-    protected JsonValidationResult validate(Schema schema, JSONObject jsonPayload) {
-        try {
-            schema.validate(jsonPayload);
-        } catch (ValidationException e) {
-            return JsonValidationResult.fromErrors(extractValidationErrors(e));
+    protected JsonValidationResult validate(JsonSchema schema, JsonNode jsonPayload) {
+        Set<ValidationMessage> validate = schema.validate(jsonPayload);
+
+        if (!validate.isEmpty()) {
+            return JsonValidationResult.fromErrors(extractValidationErrors(validate));
         }
+
         return JsonValidationResult.SUCCESS;
     }
 
-    private JSONObject createJSONObject(Object bean) {
-        if (bean instanceof JSONObject) {
-            return (JSONObject) bean;
-        }
-        if (bean instanceof byte[]) {
-            return new JSONObject(new JSONTokener(new ByteArrayInputStream((byte[]) bean)));
-        }
-        if (bean instanceof String) {
-            return new JSONObject(new JSONTokener((String) bean));
-        }
-        if (bean instanceof InputStream) {
-            return new JSONObject(new JSONTokener((InputStream) bean));
-        }
-        if (bean instanceof Map) {
-            return new JSONObject((Map<?, ?>)bean);
-        }
-        return new JSONObject(bean);
+    private JsonNode createJSONObject(Object bean) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.convertValue(bean, JsonNode.class);
     }
 
-    private List<ValidationError> extractValidationErrors(ValidationException validationException) {
+    private List<ValidationError> extractValidationErrors(Set<ValidationMessage> validationErrors) {
         List<ValidationError> errors = new ArrayList<>();
-        List<ValidationException> causes = validationException.getCausingExceptions();
-        if (causes != null && !causes.isEmpty()) {
-            for (ValidationException cause : causes) {
-                errors.addAll(extractValidationErrors(cause));
+        if (validationErrors != null && !validationErrors.isEmpty()) {
+            for (ValidationMessage cause : validationErrors) {
+                ValidationError error = new ValidationError(cause.getMessage(), cause.getCode());
+                errors.add(error);
             }
-        } else {
-            ValidationError error = new ValidationError(validationException.getMessage(), validationException.getPointerToViolation());
-            errors.add(error);
         }
         return errors;
     }
 
-    public static class JsonSchemaParser implements SchemaParser<Schema, Object> {
+    public static class JsonSchemaParser implements SchemaParser<JsonSchema, Object> {
         @Override
         public String artifactType() {
             return ArtifactType.JSON;
         }
 
         @Override
-        public Schema parseSchema(byte[] rawSchema,  Map<String, ParsedSchema<Schema>> references) {
-            return SchemaLoader.load(new JSONObject(new JSONTokener(new ByteArrayInputStream(rawSchema))));
+        public JsonSchema parseSchema(byte[] rawSchema, Map<String, ParsedSchema<JsonSchema>> resolvedReferences) {
+            Map<String, String> referenceSchemas = new HashMap<>();
+
+            resolveReferences(resolvedReferences, referenceSchemas);
+
+            JsonSchemaFactory schemaFactory = JsonSchemaFactory
+                    .getInstance(SpecVersion.VersionFlag.V7,
+                            builder -> builder.schemaLoaders(schemaLoaders -> schemaLoaders.schemas(referenceSchemas)));
+
+            return schemaFactory.getSchema(IoUtil.toString(rawSchema));
+        }
+
+        private void resolveReferences(Map<String, ParsedSchema<JsonSchema>> resolvedReferences, Map<String, String> referenceSchemas) {
+            resolvedReferences.forEach((referenceName, schema) -> {
+                if (schema.hasReferences()) {
+                    resolveReferences(schema.getSchemaReferences()
+                            .stream()
+                            .collect(Collectors.toMap(parsedSchema -> parsedSchema.getParsedSchema().getId(), parsedSchema -> parsedSchema)), referenceSchemas);
+
+                }
+                referenceSchemas.put(schema.getParsedSchema().getId(), IoUtil.toString(schema.getRawSchema()));
+            });
         }
 
         @Override
-        public ParsedSchema<Schema> getSchemaFromData(Record<Object> data) {
+        public ParsedSchema<JsonSchema> getSchemaFromData(Record<Object> data) {
             //not supported yet?
             return null;
         }
 
         @Override
-        public ParsedSchema<Schema> getSchemaFromData(Record<Object> record, boolean dereference) {
+        public ParsedSchema<JsonSchema> getSchemaFromData(Record<Object> record, boolean dereference) {
             //not supported yet?
             return null;
         }
